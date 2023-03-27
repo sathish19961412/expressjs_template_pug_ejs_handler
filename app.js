e45cda3d3bd42b3475f4df45871fdda5de13bdca
sathish19961412 <sathishfreelanc5@gmail.com>
2023-03-27 12:18:23 +0530

    expressjs templete engine initial

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname,'public')))
+
+router.get('/',(req,res,next)=>{
+    res.sendFile(path.join(__dirname,'views','index.html'));
+})
+router.get('/about',(req,res,next)=>{
+    res.sendFile(path.join(__dirname,'views','about.html'));
+})
+
+router.use((req,res,next)=>{
+    res.sendFile(path.join(__dirname,'views','404.html'));
+})
+
+app.use(router);
+
+app.listen(8000,() => {
+    console.log('Running on 8000')
+})
