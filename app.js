35f19b32d3f45c08596977ec81e1353ae1d56527
sathish19961412 <sathishfreelanc5@gmail.com>
2023-04-11 09:47:35 +0530

    Add to status code

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ const path = require('path');
 app.use(express.static(path.join(__dirname,'public')))
 
 router.get('/',(req,res,next)=>{
-    res.sendFile(path.join(__dirname,'views','index.html'));
+    res.status(200).sendFile(path.join(__dirname,'views','index.html'));
 })
 router.get('/about',(req,res,next)=>{
-    res.sendFile(path.join(__dirname,'views','about.html'));
+    res.status(200).sendFile(path.join(__dirname,'views','about.html'));
 })
 
 router.use((req,res,next)=>{
-    res.sendFile(path.join(__dirname,'views','404.html'));
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 })
 
 app.use(router);
