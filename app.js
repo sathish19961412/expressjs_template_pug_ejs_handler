c1034acd59b027ec299ca8c3fd76967bfcfebac4
sathish19961412 <sathishfreelanc5.com>
2024-07-23 01:20:35 -0400

    pug,ejs,express-handlebars template engine compelted

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const path = require('path');
+const {engine}=require('express-handlebars');
 
 app.use(express.static(path.join(__dirname,'public')))
-
-//Template Engine Pug Set
-app.set('view engine','ejs');
+app.engine('hbs',engine({
+    extname:'.hbs',
+    defaultLayout:'main'
+}))
+app.set('view engine','pug');
 
 router.get('/',(req,res,next)=>{
     const courses=[
@@ -21,7 +24,9 @@ router.get('/',(req,res,next)=>{
     res.status(200).render('index',{
         docTitle: "Welcome To Sathish",
         courses,
-        path:'index'
+        path:'index',
+        courseExists:courses.length>0,
+        pageIndex:true
     })
 })
 router.get('/about',(req,res,next)=>{
@@ -38,14 +43,17 @@ router.get('/about',(req,res,next)=>{
         active:true,
         docTitle: "About Page",
         courses,
-        path:'about'
+        path:'about',
+        courseExists:courses.length>0,
+        pageAbout:true
     });
 })
 
 router.use((req,res,next)=>{
     res.status(200).render('404',{
         docTitle:'404 Page Not Found',
-        path:'404'
+        path:'404',
+        page404:true
     });
     // res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 })
